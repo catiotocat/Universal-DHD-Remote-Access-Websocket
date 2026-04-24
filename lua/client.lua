bd1d5bb3d0cae9c5250d370024e3f252f5ce4e40
Isaac O'Rourke <22isaaco4567@gmail.com>
2026-04-24 14:03:12 -0500

    bump version

--- lua/client.lua
+++ lua/client.lua
@@ -1,6 +1,6 @@
 -- This program was designed to run inside of CraftOS-PC
 -- You can download CraftOS-PC from https://www.craftos-pc.cc/
-local programVersion = "2.7.3"
+local programVersion = "2.7.4"
 
 if not term then --Check if the program is running inside CraftOS-PC
 	print("This program was designed to run inside of CraftOS-PC")
