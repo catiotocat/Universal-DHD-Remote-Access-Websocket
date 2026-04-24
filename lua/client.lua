42fea1ee005b270d9574133a50f1c2aff7307ad7
Isaac O'Rourke <22isaaco4567@gmail.com>
2026-04-24 09:52:13 -0500

    fix version string

--- lua/client.lua
+++ lua/client.lua
@@ -1,6 +1,6 @@
 -- This program was designed to run inside of CraftOS-PC
 -- You can download CraftOS-PC from https://www.craftos-pc.cc/
-local programVersion = "2.7.0"
+local programVersion = "2.6.5"
 
 if not term then --Check if the program is running inside CraftOS-PC
 	print("This program was designed to run inside of CraftOS-PC")
